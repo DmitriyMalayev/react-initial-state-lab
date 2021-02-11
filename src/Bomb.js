fba426db4a7fbc8648bb64e4cc9c28ea56e24f9e
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-02-11 02:05:19 -0500

    commit tree 6b3e5a4fe8186998a8837b19115015b7e7df0188

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,43 @@
-// your Bomb code here!
+import React from "react";
+class Bomb extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      secondsLeft: props.initialCount,
+    };
+  }
+
+  render() {
+    //   console.log(this.props.initialCount)
+    if(this.props.initialCount != 0){
+        return <div>{this.props.initialCount} seconds left before I go boom!</div>;    
+    } else{ 
+        
+        return "Boom!"
+}
+  }
+}
+
+export default Bomb;
+// import React from "react";
+// class ImageSlider extends React.Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       currentSlideIndex: 0,
+//     };
+//   }
+//   currentSlideIndex = () => {
+//     currentSlide = this.state.count = 0;
+//     this.setState({
+//       count: currentSlide,
+//     });
+//   };
+
+//   render() {
+//     return `I am on slide ${this.state.currentSlideIndex}`;
+//   }
+// }
+
+// export default ImageSlider;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,24 @@
-// your ImageSlider code here!
+import React from 'react'
+class ImageSlider extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentSlideIndex: 0,
+    };
+  }
+  currentSlideIndex = () => {
+    currentSlide = this.state.count = 0;
+    this.setState({
+      count: currentSlide,
+    });
+  };
+
+
+  
+  render() {
+    return `I am on slide ${this.state.currentSlideIndex}`;
+  }
+}
+
+export default ImageSlider
